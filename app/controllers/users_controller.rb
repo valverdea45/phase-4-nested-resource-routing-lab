28859b30146a8766121410813446800406dc91d4
Adrian <adrianvalverde45@gmail.com>
2023-08-02 14:04:01 -0400

    commit tree 43dc6c30d76ac024790af83db4311cb1a865fac3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
 
+
+
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find(params[:id])
     render json: user, include: :items
   end
 
 end
+
+# class DogHousesController < ApplicationController
+#   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+#   def show
+#     dog_house = DogHouse.find(params[:id])
+#     render json: dog_house
+#   end
+
+#   private
+
+#   def render_not_found_response
+#     render json: { error: "Dog house not found" }, status: :not_found
+#   end
+
+# end
